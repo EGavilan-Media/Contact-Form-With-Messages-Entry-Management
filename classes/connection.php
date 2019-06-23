cd0a3f62af7626e5999a1369ca83b03d77544943
EGavilan-Media <45221488+EGavilan-Media@users.noreply.github.com>
2019-06-23 02:45:42 -0400

    Update connection.php

--- classes/connection.php
+++ classes/connection.php
@@ -1,9 +1,9 @@
 <?php
 class Connect {
-    private $server="localhost:3306";
-	private $user="homenetw_eg";
-	private $password="123456789";
-    private $db = "homenetw_contacts";
+    private $server = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $db = "egmedia_contacts";
     
     public function connection() {
         $connection = mysqli_connect($this->server, $this->user, $this->password, $this->db);
